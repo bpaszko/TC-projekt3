ed2458c29d1c6305429e7de7ae146039d0141a82
Agata Paldyna <paldyna.agata@gmail.com>
2019-05-24 21:22:22 +0200

    Passing zoom argument

--- src/website/main/scripts.js
+++ src/website/main/scripts.js
@@ -10,62 +10,69 @@ function getImgLink() {
 
 
 function previewFile(){
-   var preview = document.querySelector('img');
-   var file    = document.querySelector('input[type=file]').files[0];
-   var reader  = new FileReader();
+	var preview = document.querySelector('img');
+	var file    = document.querySelector('input[type=file]').files[0];
+	var reader  = new FileReader();
 
-   reader.onload = function () {
-      var image = new Image;
-      image.onload = function() {
-         preview.setAttribute('w', image.width)
-         preview.setAttribute('h', image.height)
-      };
-      image.src = reader.result
-      preview.src = reader.result;
-   }
+	reader.onload = function () {
+		var image = new Image;
+		image.onload = function() {
+			preview.setAttribute('w', image.width);
+			preview.setAttribute('h', image.height);
+		};
+		image.src = reader.result;
+		preview.src = reader.result;
+	}
 
-   if (file) {
-	   reader.readAsDataURL(file);
-   } 
-   else {
-	   preview.src = "";
-   }
+	if (file) {
+		reader.readAsDataURL(file);
+	} 
+	else {
+		preview.src = "";
+	}
 }
 
 
 function accessModel(){
-   var image = document.querySelector('img')
-   h = image.getAttribute('h')
-   w = image.getAttribute('w')
-   sendBase64ToServer(image.src, h, w);
+	var image = document.querySelector('img');
+	h = image.getAttribute('h');
+	w = image.getAttribute('w');
+	z = getZoom();
+	sendBase64ToServer(image.src, h, w, z);
 }
 
 
-function sendBase64ToServer(base64, h, w) {
-   var httpPost = new XMLHttpRequest(),
-       path = 'https://nsoaoib0c8.execute-api.us-east-1.amazonaws.com/dev/run_model',
-       data = JSON.stringify({image: base64, height: h, width: w});
-   httpPost.onreadystatechange = function(err) {
-           if (httpPost.readyState == 4 && httpPost.status == 200){
-               console.log(httpPost.responseText);
-			   processResponse(httpPost.responseText);
-           } else {
-               console.log(err);
-               getImgLink().innerHTML = err;
-           }
-       };
-   // Set the content type of the request to json since that's what's being sent
-   httpPost.open("POST", path, true);
-   httpPost.setRequestHeader('Content-Type', 'application/json');
-   httpPost.send(data);
+function getZoom() {
+	var radioForm = document.getElementById('radioForm');
+	return radioForm.elements['zoom'].value || 2;
+}
+
+
+function sendBase64ToServer(base64, h, w, z) {
+	var httpPost = new XMLHttpRequest(),
+		path = 'https://nsoaoib0c8.execute-api.us-east-1.amazonaws.com/dev/run_model',
+		data = JSON.stringify({image: base64, height: h, width: w, zoom: z});
+	httpPost.onreadystatechange = function(err) {
+			if (httpPost.readyState == 4 && httpPost.status == 200){
+				console.log(httpPost.responseText);
+				processResponse(httpPost.responseText);
+			} else {
+				console.log(err);
+				getImgLink().innerHTML = err;
+			}
+	   };
+	// Set the content type of the request to json since that's what's being sent
+	httpPost.open("POST", path, true);
+	httpPost.setRequestHeader('Content-Type', 'application/json');
+	httpPost.send(data);
 };
 
 
 function processResponse(responseText) {
-   var imgUrlObject = JSON.parse(responseText);
-   var imgUrl = imgUrlObject["img_url"];
-   getImgLink().innerHTML = imgUrl;
-   getImgLink().href = imgUrl;
-   var enlargedImg = document.getElementById('img_id');
-   enlargedImg.src = imgUrl;
+	var imgUrlObject = JSON.parse(responseText);
+	var imgUrl = imgUrlObject["img_url"];
+	getImgLink().innerHTML = imgUrl;
+	getImgLink().href = imgUrl;
+	var enlargedImg = document.getElementById('img_id');
+	enlargedImg.src = imgUrl;
 }
